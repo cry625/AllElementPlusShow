142e81309725e917ac0e2a3202b5ad919e2207c7
cry <cryhomewindows@qq.com>
2024-12-04 22:18:04 +0800

    改

--- AllElementPlusShow/src/main.js
+++ AllElementPlusShow/src/main.js
@@ -2,12 +2,19 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPersist from 'pinia-plugin-persist'
 import ElementPlus from 'element-plus'
+
 import 'element-plus/dist/index.css'
 import '@/assets/base.css' 
+import '@/assets/theme.css' 
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import './styles/dark/css-vars.css'
+import './styles/element/index.scss'
+
+
 import App from './App.vue'
 import router from './router'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import '@/styles/systemColor/light.scss'
+// import '@/styles/systemColor/light.scss'
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
